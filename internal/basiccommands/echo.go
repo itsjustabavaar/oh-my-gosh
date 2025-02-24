7a8828b18a5feb17b11f75186454409d4c4f965c
ghane_m <ghane_m@cyberno.ir>
2025-02-24 15:00:20 +0330

    Add new echo command support

---
+++ internal/basiccommands/echo.go
@@ -0,0 +1,31 @@
+package basiccommands
+
+import (
+	"os"
+	"strings"
+)
+
+type EchoState struct {
+	Output string
+}
+
+func (e *EchoState) Handler(input string) (string, *int, error) {
+	var err error
+	echoArgument := strings.TrimPrefix(input, "echo ")
+	if strings.HasPrefix(echoArgument, "'") && strings.HasSuffix(echoArgument, "'") {
+		return strings.Trim(echoArgument, "'"), nil, err
+	} else {
+		echoPhrases := strings.Split(echoArgument, " ")
+		for idx, phrase := range echoPhrases {
+			if strings.Contains(phrase, "$") {
+				echoComponents := strings.Split(phrase, "$")
+				for i := 1; i < len(echoComponents); i++ {
+					echoComponents[i] = os.Getenv(echoComponents[i])
+				}
+				phrase = strings.Join(echoComponents, "")
+			}
+			echoPhrases[idx] = phrase
+		}
+		return strings.Join(echoPhrases, " "), nil, err
+	}
+}
