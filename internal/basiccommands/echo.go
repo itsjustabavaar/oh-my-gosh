631acbfe50276a68b22a57e7b921958192aa268e
itsjustabavaar <ghanematin222@gmail.com>
2025-02-26 14:13:37 +0330

    add double quoted and escape characters supporting in echo command

--- internal/basiccommands/echo.go
+++ internal/basiccommands/echo.go
@@ -15,6 +15,9 @@ func (e *EchoCommand) Handler() (string, *int, error) {
 	if strings.HasPrefix(echoArgument, "'") && strings.HasSuffix(echoArgument, "'") {
 		return strings.ReplaceAll(echoArgument, "'", ""), nil, nil
 	}
+	if strings.HasPrefix(echoArgument, "\"") && strings.HasSuffix(echoArgument, "\"") {
+		return processDoubleQuotedStrings(echoArgument), nil, nil
+	}
 
 	echoPhrases := strings.Split(echoArgument, " ")
 
@@ -34,4 +37,57 @@ func (e *EchoCommand) Handler() (string, *int, error) {
 	}
 
 	return strings.Join(echoPhrases, " "), nil, nil
+}
+
+func processDoubleQuotedStrings(input string) string {
+	innerContent := input[1 : len(input)-1]
+
+	result := ""
+	escaping := false
+
+	for i := 0; i < len(innerContent); i++ {
+		c := innerContent[i]
+
+		if escaping {
+			if c == '$' || c == '`' || c == '"' || c == '\\' || c == '\n' {
+				result += string(c)
+			} else {
+				result += "\\" + string(c)
+			}
+			escaping = false
+		} else if c == '\\' {
+			escaping = true
+		} else if c == '$' {
+			if i+1 < len(innerContent) {
+				varStart := i + 1
+				varEnd := varStart
+
+				for varEnd < len(innerContent) &&
+					(innerContent[varEnd] >= 'a' && innerContent[varEnd] <= 'z' ||
+						innerContent[varEnd] >= 'A' && innerContent[varEnd] <= 'Z' ||
+						innerContent[varEnd] >= '0' && innerContent[varEnd] <= '9' ||
+						innerContent[varEnd] == '_') {
+					varEnd++
+				}
+
+				if varEnd > varStart {
+					varName := innerContent[varStart:varEnd]
+					result += os.Getenv(varName)
+					i = varEnd - 1
+				} else {
+					result += string(c)
+				}
+			} else {
+				result += string(c)
+			}
+		} else {
+			result += string(c)
+		}
+	}
+
+	if escaping {
+		result += "\\"
+	}
+
+	return result
 }
